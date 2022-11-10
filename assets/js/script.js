fa580829c7b2398e6ca8aaff38b17fdaa6bb7813
TemplarManatee <dylanbrad10@gmail.com>
2022-11-10 14:40:42 -0500

    1 question loads - working on quiz functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Grabbing HTML elements
 var quizContent = document.getElementById('quiz-content'); 
 var initBtn = document.getElementById('init-btn'); 
 var timeEl = document.getElementById('timer');
@@ -20,8 +21,9 @@ var question1 = new quizQuestion("What is programming?",0,"Pain","Suffering","To
 var question2 = new quizQuestion("JavaScript?", 1, "Ye.","Nah.","YEAH!","Absolutely not."); 
 var question3 = new quizQuestion("What's the air speed velocity of a coconut-laden swallow?",1,"Is it an African swallow?","37km^2/h","Iunno","I fart in your general direction.");
 var question4 = new quizQuestion("Is this a question.",2,"Yes","No","Sure","Why not?");
-var question5 = 
+var question5 = new quizQuestion("Freebie? Pick 2.",2,"Nope","Nope","Yep","Nope");
 
+var questionsAnswered = 0; 
 
 //Function to randomize quiz order. 
 function randomizeQuiz(fullQuiz) {
@@ -32,10 +34,7 @@ function randomizeQuiz(fullQuiz) {
 } 
 
 var fullQuiz = [question1, question2, question3, question4, question5];
-randomizeQuiz(fullQuiz); 
-
-//Function for beginning quiz
-
+var randQuiz = randomizeQuiz(fullQuiz); 
 
 //Object containing the high score: score and name 
 class scoreBoard {
@@ -45,29 +44,43 @@ class scoreBoard {
 
 //Initialize quiz on button press
 initBtn.addEventListener("click", function() {
-    quizContent.innerHTML = null;
-    var promptHeader = document.createElement("h2");
-    var answerChoicesUl = document.createElement("ul");
-    promptHeader.textContent = "Hi";
-    quizContent.appendChild(promptHeader);
-    quizContent.appendChild(answerChoicesUl); 
+    buildQuizElements(1);
     setTimer();
 })
 
+//Resets the quiz-element div and builds the quiz question in its place
+function buildQuizElements(questionIndex) {
+    quizContent.innerHTML = null;
+    var promptHeader = document.createElement("h2");
+    var answerChoicesUl = document.createElement("ul");
+    answerChoicesUl.id = 'answer-choices';
+    promptHeader.textContent = randQuiz[questionIndex].prompt;
+    quizContent.appendChild(promptHeader); 
+    quizContent.appendChild(answerChoicesUl);
+    //Create answer choices
+    for(i=0; i < randQuiz[questionIndex].answerChoices.length; i++) {
+        console.log(i);
+        var answerChoice = document.createElement("li");
+        var answerButton = document.createElement("button");
+        answerButton.textContent = randQuiz[questionIndex].answerChoices[i]; 
+        answerChoice.appendChild(answerButton); 
+        quizContent.appendChild(answerChoice);
+    }
+    
+}
+
+var secondsLeft = 70; 
 //  Begin timer countdown 
 function setTimer(){
     var timerInterval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+        timeEl.textContent = "Timer: " + secondsLeft;
     
         if(secondsLeft === 0) {
-          // Stops execution of action at set interval
           clearInterval(timerInterval);
-          // Calls function to create and append image
-          sendMessage();
+          //buildScoreboard();
         }
-    
-      }, 7000);
+      }, 1000);
 }
 
 //  Replace 'quiz-content' div with quiz question 
